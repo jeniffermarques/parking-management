e7d326fd6011dc0792d883797c074ca0ba82a6ec
Jeniffer Marques <genes.marques.silva@gmail.com>
2020-11-21 00:10:35 -0300

    Adicionando funcionalidade no botão deletar

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-"use strict";
-var __spreadArrays = (this && this.__spreadArrays) || function () {
-    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
-    for (var r = Array(s), k = 0, i = 0; i < il; i++)
-        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
-            r[k] = a[j];
-    return r;
-};
 (function () {
     var _a;
-    var $ = function (query) {
-        return document.querySelector(query);
-    };
+    const $ = (query) => document.querySelector(query);
+    function calculaTempo(mil) {
+        const minuto = Math.floor(mil / 60000);
+        const segundo = Math.floor((mil % 60000) / 1000);
+        return `${minuto}m e ${segundo}s`;
+    }
     function patio() {
         function ler() {
             return localStorage.patio ? JSON.parse(localStorage.patio) : [];
@@ -19,32 +14,49 @@ var __spreadArrays = (this && this.__spreadArrays) || function () {
             localStorage.setItem('patio', JSON.stringify(veiculos));
         }
         function adicionar(veiculo, salva) {
-            var _a;
-            var row = document.createElement('tr');
-            row.innerHTML = "\n                <td>" + veiculo.nome + "</td>\n                <td>" + veiculo.placa + "</td>\n                <td>" + veiculo.entrada + "</td>\n                <td>\n                    <button class=\"delete\" data-placa=\"" + veiculo.placa + "\">X</button>\n                </td>               \n            ";
-            (_a = $('#patio')) === null || _a === void 0 ? void 0 : _a.appendChild(row);
+            var _a, _b;
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${veiculo.nome}</td>
+                <td>${veiculo.placa}</td>
+                <td>${veiculo.entrada}</td>
+                <td>
+                    <button class="delete" data-placa="${veiculo.placa}">X</button>
+                </td>               
+            `;
+            (_a = row.querySelector('.delete')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', function () {
+                remover(this.dataset.placa);
+            });
+            (_b = $('#patio')) === null || _b === void 0 ? void 0 : _b.appendChild(row);
             if (salva)
-                salvar(__spreadArrays(ler(), [veiculo]));
+                salvar([...ler(), veiculo]);
         }
-        function remover() { }
+        function remover(placa) {
+            const { entrada, nome } = ler().find(veiculo => veiculo.placa === placa);
+            const tempo = calculaTempo(new Date().getTime() - new Date(entrada).getTime());
+            if (!confirm(`O veículo ${nome} permaneceu por ${tempo}. Deseja encerrar? `))
+                return;
+            salvar(ler().filter((veiculo) => veiculo.placa !== placa));
+            render();
+        }
         function render() {
             $('#patio').innerHTML = '';
-            var patio = ler();
+            const patio = ler();
             if (patio.length) {
-                patio.forEach(function (veiculo) { return adicionar(veiculo); });
+                patio.forEach(veiculo => adicionar(veiculo));
             }
         }
-        return { ler: ler, adicionar: adicionar, remover: remover, salvar: salvar, render: render };
+        return { ler, adicionar, remover, salvar, render };
     }
     patio().render();
-    (_a = $("#cadastrar")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", function () {
+    (_a = $("#cadastrar")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", () => {
         var _a, _b;
-        var nome = (_a = $('#nome')) === null || _a === void 0 ? void 0 : _a.value;
-        var placa = (_b = $('#placa')) === null || _b === void 0 ? void 0 : _b.value;
+        const nome = (_a = $('#nome')) === null || _a === void 0 ? void 0 : _a.value;
+        const placa = (_b = $('#placa')) === null || _b === void 0 ? void 0 : _b.value;
         if (!nome || !placa) {
             alert("Os campos nome e placa são obrigatórios");
             return;
         }
-        patio().adicionar({ nome: nome, placa: placa, entrada: new Date() }, true);
+        patio().adicionar({ nome, placa, entrada: new Date().toISOString() }, true);
     });
 })();
--- script.ts
+++ script.ts
@@ -1,13 +1,20 @@
     interface Veiculo {
         nome: string;
         placa: string;
-        entrada: Date;
+        entrada: Date | string;
     }
 
 
 (function () {
   const $ = (query: string): HTMLInputElement | null =>
     document.querySelector(query);
+
+    function calculaTempo(mil : number) {
+        const minuto = Math.floor(mil / 60000);
+        const segundo = Math.floor((mil % 60000)/ 1000);
+
+        return `${minuto}m e ${segundo}s`;
+    }        
 
     function patio() {
         function ler (): Veiculo[] {
@@ -30,12 +37,27 @@
                 </td>               
             `;
 
+            row.querySelector('.delete')?.addEventListener('click', function(){
+                remover(this.dataset.placa)
+            })
+
             $('#patio')?.appendChild(row);
 
             if(salva) salvar([...ler(), veiculo]);
         }
 
-        function remover(){}        
+        function remover(placa: string){
+
+            const {entrada, nome} = ler().find(veiculo => veiculo.placa === placa);
+
+            const tempo = calculaTempo(new Date().getTime() - new Date(entrada).getTime());
+
+            if(!confirm(`O veículo ${nome} permaneceu por ${tempo}. Deseja encerrar? `)) 
+            return;
+
+            salvar(ler().filter((veiculo) => veiculo.placa !== placa));
+            render();
+        }        
 
         function render(){
             $('#patio')!.innerHTML = '';
@@ -59,6 +81,6 @@
       alert("Os campos nome e placa são obrigatórios");
       return;
     }
-     patio().adicionar({nome, placa, entrada: new Date()}, true);
+     patio().adicionar({nome, placa, entrada: new Date().toISOString()}, true);
   });
 })();
